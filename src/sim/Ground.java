8d9329e15994edb1ebbba119baad671111c3a4e8
Cyclical- <jonahshaps@gmail.com>
2017-01-22 17:32:05 -0500

    Final changes

--- src/sim/Ground.java
+++ src/sim/Ground.java
@@ -24,41 +24,56 @@ public class Ground{
         this.world = world;
     }
 
+    /**
+     * createGround
+     * @author Jonah Shapiro
+     * @description Randomly generates a set of tiles
+     */
     public void createGround(){
-        Vec2 tilePos = new Vec2(0, -0.5f);
+        Vec2 tilePos = new Vec2(0, -0.5f); //the initial tile position
+        //the following code generates 4 flat tiles to ensure a fair start
         for (int k = 0; k < 4; k++){
-            Body start = newTile(tilePos, 0f);
-            tilePos = start.getWorldPoint(this.newCoordinates.get(3));
+            Body start = newTile(tilePos, 0f); //create the first tile
+            tilePos = start.getWorldPoint(this.newCoordinates.get(3)); //get the next tile coordinates
         }
         for (int i = 0; i < maxSegments-4; i++){
-            Body previousTile = newTile(tilePos, (float)((Util.nextFloat(-10f, 8f) * 8f / 100) * Math.pow(-1, i)));
-            tilePos = previousTile.getWorldPoint(this.newCoordinates.get(3));
+            Body previousTile = newTile(tilePos, (float)((Util.nextFloat(-10f, 8f) * 8f / 100) * Math.pow(-1, i))); //generate a new tile
+            tilePos = previousTile.getWorldPoint(this.newCoordinates.get(3)); //get the next tile coordinates
         }
 
     }
 
 
+    /**
+     * newTile
+     * @author Jonah Shapiro
+     * @description creates a new tile
+     * @param pos the position of this tile
+     * @param angle the angle of this tile
+     * @return the newly created tile body
+     */
     private Body newTile(Vec2 pos, float angle){
         BodyDef bodyDef = new BodyDef();
         bodyDef.setPosition(new Vec2(pos.x, pos.y));
-        Body body = world.createBody(bodyDef);
+        Body body = world.createBody(bodyDef); //create the tile body
 
-        Vec2[] coordinates = { //create the initial segment
+        Vec2[] coordinates = { //create the initial tile coordinates
                 new Vec2(0,0),
                 new Vec2(0, -segmentHeight),
                 new Vec2(segmentLength, -segmentHeight),
                 new Vec2(segmentLength, 0)
         };
         Vec2 center = new Vec2(0, 0);
-        newCoordinates = rotate(coordinates, center, angle);
-        PolygonShape segment = new PolygonShape();
-
-        segment.set(newCoordinates.toArray(new Vec2[0]), newCoordinates.size());
+        
+        newCoordinates = rotate(coordinates, center, angle); //rotate the tile coordinates based on the angle
+       
+        PolygonShape segment = new PolygonShape();   
+        segment.set(newCoordinates.toArray(new Vec2[0]), newCoordinates.size()); //add the rotated tile coordinates to the shape
         FixtureDef fixture = new FixtureDef();
         fixture.setFriction(0.5f);
-        fixture.setShape(segment);
+        fixture.setShape(segment); //attach the shape to the fixture
 
-        body.createFixture(fixture);
+        body.createFixture(fixture); //attach the fixture to the shape
         return body;
 
 
